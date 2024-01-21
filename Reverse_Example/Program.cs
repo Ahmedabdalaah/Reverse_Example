11996e4dee94f9cf11d7a7e2900beee1cc0b9691
Ahmed <ahmed.ismail14141414@gmail.com>
2024-01-21 03:03:49 +0200

    Add project files.

---
+++ Reverse_Example/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Reverse_Example;
+Revers rever = new Revers();
+Console.WriteLine("Enter Input");
+string input = Console.ReadLine();
+if(input != "")
+{
+    try
+    {
+        rever.rev(input);
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+else
+{
+    Console.WriteLine("invalid input data");
+}
---
+++ Reverse_Example/Revers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Example
+{
+    internal class Revers
+    {
+        public void rev(string input)
+        {
+            string output = "";
+            for(int x=input.Length-1;x>=0 ;x--)
+            {
+                output += input[x];
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
